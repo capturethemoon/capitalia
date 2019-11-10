ebef0c5dc9e38ebdb34163a52a6933ca578c2dc8
Jamie Ferrugiaro <missjamiekaren@gmail.com>
2019-11-09 23:08:27 -0500

    Got value of treasury, following with rest of form.

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,52 @@
 console.log("Working...");
 
+let depositOptions = [
+    {
+        name: 'bank',
+        amount: 0,
+        rating: 1
+    },
+    {
+        name: 'treasury',
+        amount: 0,
+        rating: 2
+    },
+    {
+        name: 'bond',
+        amount: 0,
+        rating: 3,
+    },
+    {
+        name: 'mutal',
+        amount: 0,
+        rating: 4,
+    }
+];
+
+
+function checkDeposits(id, deposit) {
+
+    depositOptions.forEach(entry => {
+        if (id == entry.name) {
+            entry.amount = deposit;
+        }
+
+    });
+
+
+};
+
+
+// grab user submitted numbers
 $("#user-submit").on("click", (event) => {
     event.preventDefault();
     console.log("Button clicked!");
 
-    console.log(event);
+  let treasury = $("#treasury").val();
+
+  console.log(treasury);
 
     
+
+
 }); 
